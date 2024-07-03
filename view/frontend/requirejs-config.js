296f0e41bbad3fd1c1b89f8e7535f2b278f994a8
Ben <ben@fishpig.com>
2024-07-03 13:45:47 +0100

    1.0.8

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        "*": {
+            'Magento_ReCaptchaWebapiUi/js/webapiReCaptchaRegistry': 'FishPig_RemoveReCaptcha/js/webapiReCaptchaRegistry'
+        }
+    }
+};
